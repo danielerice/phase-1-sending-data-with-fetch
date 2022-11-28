d6e51167311099fe9cb51aba8c730ab093f59096
Daniel Rice <danielrice5627@gmail.com>
2022-11-28 14:44:04 -0800

    commit tree d29f615c4b4798cc0be7f50331761fd209a78b9f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData (userName, userEmail) {
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+          }),
+      })
+    .then(res => res.json())
+    .then(rtnObj => buildUserInDOM(rtnObj))
+    .catch(function (error) {
+        alert(`you goofed`);
+        let errorMessage = document.createElement(`p`);
+        errorMessage.textContent = error.message;
+        document.body.append(errorMessage);
+    })
+
+}
+function buildUserInDOM (obj) {
+   let newUser = document.createElement('div');
+   newUser.id = obj.id;
+   document.body.append(newUser);
+}
